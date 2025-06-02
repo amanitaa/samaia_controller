e7fca46de6cfdec6ea32047a12f9ba8a8443bd52
tornike <tornikegerkena02@gmail.com>
2025-06-02 11:32:16 +0400

    update: network

--- network/udp_client.py
+++ network/udp_client.py
@@ -4,11 +4,12 @@ from config import SAMAYA_PORT, SAMAYA_IP
 from utils.logger import logger
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect((SAMAYA_IP, SAMAYA_PORT))
 
 
 def send_command(msg):
     try:
-        sock.sendto(msg.encode(), (SAMAYA_IP, SAMAYA_PORT))
+        sock.send(msg.encode())
         logger.info(f"Sent: {msg}")
     except Exception as e:
         logger.error(f"Failed to send command: {e}")
@@ -17,7 +18,7 @@ def send_command(msg):
 def receive_event():
     try:
         data, _ = sock.recvfrom(1024)
-        msg = data.decode().strip().upper()
+        msg = data.decode().strip()
         logger.info(f"Received: {msg}")
         return msg
     except Exception as e:
