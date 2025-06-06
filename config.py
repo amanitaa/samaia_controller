cb2422d5eb8d322094bfe36ce9c4992fb85d96d8
tornike <tornikegerkena02@gmail.com>
2025-06-06 20:41:13 +0400

    network update

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-SAMAYA_IP: str = os.environ.get("SAMAYA_IP", "0.0.0.0")
+SAMAYA_IP: str = os.environ.get("SAMAYA_IP", "192.168.4.1")
 SAMAYA_PORT: int = os.environ.get("SAMAYA_PORT", 5000)
